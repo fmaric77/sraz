6a623eaa5d7b77029ca99f1af16710a62bed4cd1
fmaric77 <fmaric77@gmail.com>
2025-09-25 22:15:21 +0200

    dwa

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -1,7 +1,10 @@
 import Stripe from 'stripe';
 
-const sk = process.env.STRIPE_SECRET_KEY || 'sk_test_51RiZm0Q9TtpeyXJ7xFWGSfCirKnD3PsFLwp7S3VjJelXpz9synu9CvoJiulRn9yqmRcLUpg82HdETO3979PZ8IY90085ewQ5lC';
+const sk = process.env.STRIPE_SECRET_KEY;
+if (!sk) {
+  throw new Error('STRIPE_SECRET_KEY is not defined in environment variables');
+}
 
 export const stripe = new Stripe(sk);
 
-export const STRIPE_PUBLISHABLE_KEY = process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY || 'pk_test_51RiZm0Q9TtpeyXJ7dDU0QXj5ClkQmsLPyzAKXH7RgPorLR08aEjAVlVAkzJuhmHAz9b8JGejftsUYDQRXAY8j4C600mGCBNrxV';
+export const STRIPE_PUBLISHABLE_KEY = process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY;
